98db01776ff043d5fa0a6551ac187624354e3de1
Vladimir <dv.quiz@yandex.ru>
2024-01-31 20:22:09 +0300

    Revert "HW_Git_1"
    This reverts commit a47a963add3271fd2ca57b12fea3f6cff6c62dfc.

--- Sem_6/myproject/myapp/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class Choice(forms.Form):
-    action = forms.ChoiceField(choices=[('coin', 'Coin'), ('dice', 'Dice'), ('random', 'Random')])
-    tries = forms.IntegerField(min_value=1, max_value=64)
--- Sem_6/myproject/myapp/management/commands/read_author.py
+++
@@ -1,8 +0,0 @@
-from django.core.management import BaseCommand
-from myapp.models import Author
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        authors = Author.objects.all()
-        self.stdout.write(f'{authors}')
